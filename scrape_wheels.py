f0a44a5a13a775b096259add85df17364475afca
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-11-29 12:10:49 -0800

    Add filecmp as well

--- scrape_wheels.py
+++ scrape_wheels.py
@@ -20,10 +20,12 @@ import urllib.request
 import os
 from pathlib import Path
 import hashlib
+import filecmp
 
 # from shasum -a 256 libopenblasp-r0.3.0.dev.dylib
 # in the MacOS 64-bit wheel from a recent CRON job:
 known_sha256 ='2f7888f884abe46be2151b75b5ce00a9e6b839b35804f6568e7a90bed13074b7'
+reference_file = "/Users/treddy/rough_work/numpy/openblas_analysis/numpy/.dylibs/libopenblasp-r0.3.0.dev.dylib"
 url = "https://3f23b170c54c2533c070-1c8a9b3114517dc5fe17b7c3f8c63a43.ssl.cf2.rackcdn.com/"
 html_page = requests.get(url)
 soup = BeautifulSoup(html_page.content, features='html5lib')
@@ -64,6 +66,9 @@ for link in soup.findAll('a'):
                            if hashval == known_sha256:
                               num_matching_hashes += 1
                               print("hashes match!!")
+                           if filecmp.cmp(candidate_file, reference_file):
+                              print("byte-by-byte match is True")
+
                        print("-" * 20)
 
 print("Total number of matching file sizes:", num_matching_file_sizes)
