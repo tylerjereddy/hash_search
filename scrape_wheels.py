c73847f06d2b5a0a44baefac91a56f88aac99cf9
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-11-29 09:59:45 -0800

    Narrow file names printed to those with appropriate version and platform tags.

--- scrape_wheels.py
+++ scrape_wheels.py
@@ -18,4 +18,10 @@ url = "https://3f23b170c54c2533c070-1c8a9b3114517dc5fe17b7c3f8c63a43.ssl.cf2.rac
 html_page = requests.get(url)
 soup = BeautifulSoup(html_page.content, features='html5lib')
 for link in soup.findAll('a'):
-    print(link.get('href'))
+    filename = link.get('href')
+    if ('openblas' in filename and
+        '0.3.0' in filename and
+        ('Darwin' in filename or
+         'osx' in filename)):
+         # these files are all of type *.tar.gz
+         print(filename)
